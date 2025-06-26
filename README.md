ebecc3375b548e886eaf3f7ed829be34889d8c99
Tzar-T70 <183425033+Tzar-T70@users.noreply.github.com>
2025-06-26 20:41:21 +0100

    finished tut

--- README.md
+++ README.md
@@ -6,6 +6,19 @@ A simple blog site made using node.js using a tutorial from Net Ninja
 
 ## (extra things) To-Do (once finished the tutorial)
 
+### function
+- add author property to blog model
+- show date blog was created on site
 - make a simple homepage (change routes)
+
+### form
 - change css (learn css proper, find good pics, get inspo)
-- make footer
+- make footer n nav bar nicer
+- make 404 page nicer
+
+## random long term ideas
+
+- have comments on blogs (disqus mayb?)
+- polls?
+- categories on blogs?
+- users with auth????  (MASSIVE if)
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog'); //import model
+const blogRoutes = require('./routes/blogRoutes');
 
 // initialize express app
 const app = express();
@@ -22,90 +22,17 @@ app.use(express.urlencoded()); //allows form data to be used in req var in post 
 app.use(morgan('dev')); //sends browser request info in terminal
 
 
-
-//display all blogs
-app.get('/all-blogs', (req,res) => {
-  Blog.find()
-  .then((result) => {
-    res.send(result);// if no errors, send to browser
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-});
-
-app.get('/single-blog', (req,res) => {
-  Blog.findById('6857e7fdf2a4a469c9bb6a2f')
-  .then((result) => {
-    res.send(result);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-});
-
+//routes
 app.get('/', (req, res) => {
-  res.redirect('/blogs');
+  res.redirect('/blogs'); //make this a separate homepage
 });
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
 
 
-//blog routes
-app.get('/blogs', (req,res) => {
-  Blog.find().sort({createdAt: -1})
-  .then((result) => {
-    res.render('index', {title: 'All Blogs', blogs: result})
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-})
-
-
-app.post('/blogs', (req,res) => {
-  //add new blog to db
-  const blog = new Blog(req.body);
-
-  blog.save()
-  .then((result) => {
-    res.redirect('/blogs');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-});
-
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
-
-app.get('/blogs/:id', (req,res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-  .then((result) => {
-    res.render('details', {blog: result, title: 'Blog Details'});
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-});
-
-app.delete('/blogs/:id', (req,res) => {
-  const id = req.params.id;
-
-  //delete id from database
-  Blog.findByIdAndDelete(id)
-  .then(result => {
-    //cant do redirect straight up after del req, gotta work around it thru json response
-      res.json({redirect: '/blogs'});
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-})
-
+//blog routes - in separate file
+app.use('/blogs',blogRoutes);
 
 
 // 404 page
